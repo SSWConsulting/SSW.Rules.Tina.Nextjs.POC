d2ced9bc1c70e6e8d94a7134dcb97ed6e5657f73
Gilles Pothieu <gillespothieu@ssw.fr>
2025-08-29 14:37:29 +0200

    Fixing icon position, size, and z-index positionning of the dropdown

--- components/server/MegaMenuWrapper.tsx
+++ components/server/MegaMenuWrapper.tsx
@@ -26,32 +26,35 @@ const ActionButtons = () => {
     const { trackEvent } = useAppInsights();
 
     return (
-        <div className='action-btn-container max-sm:order-2 max-sm:mt-4 flex justify-items-end align-middle'>
-            <Tooltip text='Try out RulesGPT' showDelay={3000} hideDelay={18000}>
-                <a
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    href='https://rulesgpt.ssw.com.au'
-                    className='action-btn-link-underlined'
-                    onClick={() => {
-                        trackEvent('RulesGPTButtonPressed');
-                    }}
-                >
-                    <RiOpenaiFill className='header-icon' />
-                </a>
-            </Tooltip>
+        <div className='action-btn-container max-sm:order-2 max-sm:mt-4 flex justify-between items-center w-full'>
+            <div className='flex items-center'>
+                <Tooltip text='Try out RulesGPT' showDelay={3000} hideDelay={18000}>
+                    <a
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        href='https://rulesgpt.ssw.com.au'
+                        className='action-btn-link-underlined'
+                        onClick={() => {
+                            trackEvent('RulesGPTButtonPressed');
+                        }}
+                    >
+                        <RiOpenaiFill className='header-icon' />
+                    </a>
+                </Tooltip>
 
-            <Tooltip text='Create an SSW Rule'>
-                <a target='_blank' rel='noopener noreferrer' href={`/rules/admin/#/collections/rule/new`} className='action-btn-link-underlined'>
-                    <RiAddCircleFill className='header-icon' />
-                </a>
-            </Tooltip>
+                <Tooltip text='Create an SSW Rule'>
+                    <a target='_blank' rel='noopener noreferrer' href={`/rules/admin/#/collections/rule/new`} className='action-btn-link-underlined'>
+                        <RiAddCircleFill className='header-icon' />
+                    </a>
+                </Tooltip>
 
-            <Tooltip text='SSW Rules wiki'>
-                <a target='_blank' rel='noopener noreferrer' href='https://github.com/SSWConsulting/SSW.Rules.Content/wiki' className='action-btn-link-underlined'>
-                    <RiQuestionFill className='header-icon' />
-                </a>
-            </Tooltip>
+                <Tooltip text='SSW Rules wiki'>
+                    <a target='_blank' rel='noopener noreferrer' href='https://github.com/SSWConsulting/SSW.Rules.Content/wiki' className='action-btn-link-underlined'>
+                        <RiQuestionFill className='header-icon' />
+                    </a>
+                </Tooltip>
+            </div>
+            
             <SignIn />
         </div>
     );
