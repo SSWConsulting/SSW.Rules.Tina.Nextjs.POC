058d0712b3b2c9faf012ab8d5d715502ffcc2ba9
Gilles Pothieu <gillespothieu@ssw.fr>
2025-09-01 08:50:02 +0200

    Swapping css classes into Tailwind classes

--- components/layout/nav/footer.tsx
+++ components/layout/nav/footer.tsx
@@ -13,14 +13,14 @@ const commitHash = process.env.COMMIT_HASH;
 export const Footer = () => {
     return (
         <>
-            <div className='p-4 text-center bg-grey-translucent text-sm'>
+            <div className='p-4 text-center bg-gray-500/10 text-sm'>
                 <section className='main-container flex max-sm:flex-col items-center justify-center gap-2'>
                     <span>
                         We <FaHeart className='text-ssw-red inline' size={12} /> open source.
                     </span>
                     <span>
                         Loving SSW Rules?{' '}
-                        <a href='https://github.com/SSWConsulting/SSW.Rules' target='_blank' rel='noreferrer' className='action-button-label footer-greybar-link'>
+                        <a href='https://github.com/SSWConsulting/SSW.Rules' target='_blank' rel='noreferrer' className='action-button-label underline hover:underline'>
                             Star us on GitHub.
                         </a>{' '}
                     </span>
@@ -33,32 +33,73 @@ export const Footer = () => {
                         <div className='mx-6 flex flex-col-reverse md:flex-row justify-between align-middle leading-6'>
                             <div className='py-2'>&copy; 1990-{new Date().getFullYear()} SSW. All rights reserved.</div>
                             <div className='w-full md:w-3/6 md:text-right py-2 flex max-sm:flex-col max-sm:items-start items-center max-sm:justify-start justify-center md:justify-end'>
-                                <a className='footer-link max-sm:mb-3' href='https://github.com/SSWConsulting/SSW.Rules/issues'>
+                                <a
+                                    className='inline-block text-white visited:text-white leading-3 transition-all duration-300 ease-in-out hover:text-ssw-red max-sm:mb-3'
+                                    href='https://github.com/SSWConsulting/SSW.Rules/issues'
+                                >
                                     FEEDBACK / SUGGEST A FEATURE
                                 </a>
                                 <span className='px-2 hidden sm:inline'>|</span>
-                                <a className='footer-link max-sm:mb-3' href='https://www.ssw.com.au/terms-and-conditions'>
+                                <a
+                                    className='inline-block text-white visited:text-white leading-3 transition-all duration-300 ease-in-out hover:text-ssw-red max-sm:mb-3'
+                                    href='https://www.ssw.com.au/terms-and-conditions'
+                                >
                                     TERMS AND CONDITIONS
                                 </a>
                                 <span className='px-2 hidden sm:inline'>|</span>
                                 <div className='inline-flex flex-row-reverse justify-end flex-nowrap'>
-                                    <a className='unstyled social-media-icon' id='tiktok-icon' title='SSW on TikTok' href='https://www.tiktok.com/@ssw_tv' target='_blank' rel='noreferrer'>
+                                    <a
+                                        className='unstyled block float-right h-[25px] w-[25px] ml-2 text-white visited:text-white no-underline text-center leading-[25px]'
+                                        id='tiktok-icon'
+                                        title='SSW on TikTok'
+                                        href='https://www.tiktok.com/@ssw_tv'
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
                                         <FaTiktok size={24} />
                                     </a>
-                                    <a className='unstyled social-media-icon' id='twitter-icon' title='SSW on Twitter' href='https://twitter.com/SSW_TV' target='_blank' rel='noreferrer'>
+                                    <a
+                                        className='unstyled block float-right h-[25px] w-[25px] ml-2 text-white visited:text-white no-underline text-center leading-[25px]'
+                                        id='twitter-icon'
+                                        title='SSW on Twitter'
+                                        href='https://twitter.com/SSW_TV'
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
                                         <FaXTwitter size={24} />
                                     </a>
-                                    <a className='unstyled social-media-icon' id='instagram-icon' title='SSW on Instagram' href='https://www.instagram.com/ssw_tv' target='_blank' rel='noreferrer'>
+                                    <a
+                                        className='unstyled block float-right h-[25px] w-[25px] ml-2 text-white visited:text-white no-underline text-center leading-[25px]'
+                                        id='instagram-icon'
+                                        title='SSW on Instagram'
+                                        href='https://www.instagram.com/ssw_tv'
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
                                         <FaInstagram size={24} />
                                     </a>
-                                    <a className='unstyled social-media-icon' id='facebook-icon' title='SSW on Facebook' href='https://www.facebook.com/SSW.page' target='_blank' rel='noreferrer'>
+                                    <a
+                                        className='unstyled block float-right h-[25px] w-[25px] ml-2 text-white visited:text-white no-underline text-center leading-[25px]'
+                                        id='facebook-icon'
+                                        title='SSW on Facebook'
+                                        href='https://www.facebook.com/SSW.page'
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
                                         <FaFacebook size={24} />
                                     </a>
-                                    <a className='unstyled social-media-icon' id='linkedin-icon' title='SSW on LinkedIn' href='https://www.linkedin.com/company/ssw' target='_blank' rel='noreferrer'>
+                                    <a
+                                        className='unstyled block float-right h-[25px] w-[25px] ml-2 text-white visited:text-white no-underline text-center leading-[25px]'
+                                        id='linkedin-icon'
+                                        title='SSW on LinkedIn'
+                                        href='https://www.linkedin.com/company/ssw'
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
                                         <FaLinkedin size={24} />
                                     </a>
                                     <a
-                                        className='unstyled social-media-icon'
+                                        className='unstyled block float-right h-[25px] w-[25px] ml-2 text-white visited:text-white no-underline text-center leading-[25px]'
                                         id='youtube-icon'
                                         title='SSW on YouTube'
                                         href='https://www.youtube.com/user/sswtechtalks'
@@ -74,23 +115,37 @@ export const Footer = () => {
                         <div className='flex flex-col md:flex-row justify-between mx-6'>
                             <div className='py-2'>
                                 This website is under{' '}
-                                <a className='footer-link' href={`${pathPrefix}/rules-to-better-websites-deployment`}>
+                                <a
+                                    className='inline-block text-white visited:text-white leading-3 transition-all duration-300 ease-in-out hover:text-ssw-red'
+                                    href={`${pathPrefix}/rules-to-better-websites-deployment`}
+                                >
                                     CONSTANT CONTINUOUS DEPLOYMENT
                                 </a>
                                 . Last deployed {getLastDeployTime()} ago
                                 {buildDate && <span title={buildDate}> on {moment(buildDate).format('MMM D, YYYY [at] HH:mm UTC')}</span>} (Build #{' '}
-                                <a className='footer-link' href={deploymentUrl} target='_blank' rel='noreferrer'>
+                                <a
+                                    className='inline-block text-white visited:text-white leading-3 transition-all duration-300 ease-in-out hover:text-ssw-red'
+                                    href={deploymentUrl}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                >
                                     {versionDeployed}
                                 </a>
                                 {commitHash && <span title={`Commit: ${commitHash}`}>-{commitHash}</span>})
                             </div>
                             <div className='md:text-right py-2'>
                                 Powered by{' '}
-                                <a className='footer-link' href={`${pathPrefix}/rules-to-better-azure`}>
+                                <a
+                                    className='inline-block text-white visited:text-white leading-3 transition-all duration-300 ease-in-out hover:text-ssw-red'
+                                    href={`${pathPrefix}/rules-to-better-azure`}
+                                >
                                     Azure
                                 </a>{' '}
                                 and{' '}
-                                <a className='footer-link' href={`${pathPrefix}/static-site-generator`}>
+                                <a
+                                    className='inline-block text-white visited:text-white leading-3 transition-all duration-300 ease-in-out hover:text-ssw-red'
+                                    href={`${pathPrefix}/static-site-generator`}
+                                >
                                     {' '}
                                     GitHub
                                 </a>
