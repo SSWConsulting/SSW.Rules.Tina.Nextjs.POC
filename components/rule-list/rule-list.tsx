38549c5fefee5661f91e8f6c368c556a20c7da7c
Hugo Pernet <hugopernet@ssw.fr>
2025-09-02 11:15:11 +0200

    Data - fixing duplicate key issues on the category page

--- components/rule-list/rule-list.tsx
+++ components/rule-list/rule-list.tsx
@@ -68,7 +68,7 @@ const RuleList: React.FC<RuleListProps> = ({ categoryUri, rules, type, noContent
       <ol className="flex flex-col justify-between gap-4 p-0 list-none">
         {rules.map((rule, i) => (
           <RuleListItem
-            key={rule.guid}
+           key={`${rule.guid}-${rule.uri}`}
             rule={rule}
             index={i}
             onBookmarkRemoved={onBookmarkRemoved}
