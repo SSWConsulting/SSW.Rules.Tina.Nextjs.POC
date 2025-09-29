47f4ed47353c99088925fd4c31bded3bc444c035
Hugo Pernet <hugopernet@ssw.fr>
2025-09-29 17:36:42 +0200

    Front - updating orphaned page

--- app/orphaned/client-page.tsx
+++ app/orphaned/client-page.tsx
@@ -7,6 +7,7 @@ import { getAccessToken } from "@auth0/nextjs-auth0";
 import { BookmarkService } from "@/lib/bookmarkService";
 import { useAuth } from "@/components/auth/UserClientProvider";
 import { Rule } from "@/models/Rule";
+import { PiWarningFill } from "react-icons/pi";
 
 export default function OrphanedClientPage(props) {
   const { orphanedRules } = props;
@@ -69,9 +70,7 @@ export default function OrphanedClientPage(props) {
           
           {/* Warning alert card */}
           <div className="mt-4 bg-red-50 border border-red-200 rounded-lg p-4 mb-4 flex items-center">
-            <svg className="w-5 h-5 mr-3 flex-shrink-0 text-red-600" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-            </svg>
+            <PiWarningFill className="inline text-ssw-red mr-2" size={18} />
             <span className="text-red-800">The rules listed below have no parent category</span>
           </div>
 
