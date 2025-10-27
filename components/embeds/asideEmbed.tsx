27af1c5edf2a70a48142855bb49417f30159d23b
Jake Bayliss <bayliss.jw@gmail.com>
2025-10-27 17:21:08 +1100

    Change link colour to black

--- components/embeds/asideEmbed.tsx
+++ components/embeds/asideEmbed.tsx
@@ -93,7 +93,7 @@ export function AsideEmbed({ data }: { data: any }) {
         <div className={`p-4 rounded-sm my-4 ${config.containerClass}`}>
             <div className="flex items-center">
             {config.icon}
-            <div className={`prose prose-neutral prose-sm max-w-none text-base ${config.textClass ?? ""}`}>
+            <div className={`max-w-none text-base ${config.textClass ?? ""}`}>
                 <TinaMarkdown content={data.body} components={MarkdownComponentMapping} />
             </div>
             </div>
@@ -101,7 +101,6 @@ export function AsideEmbed({ data }: { data: any }) {
       </ComponentWithFigure>
     );
 }
-  
 
 export const asideEmbedTemplate: Template = withFigureEmbedTemplateFields({
   name: "asideEmbed",
