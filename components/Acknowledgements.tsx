80237fdee5c6cb4b4bbd2783c577a817c0256e4d
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-10-28 10:01:12 +1100

    adding additional guard for the image src

--- components/Acknowledgements.tsx
+++ components/Acknowledgements.tsx
@@ -83,15 +83,17 @@ export default function Acknowledgements({ authors }: AcknowledgementsProps) {
           >
             <div className="w-12 h-12 overflow-hidden rounded-full relative">
               <a href={author.url} target="_blank" rel="noopener noreferrer">
-                <Image
-                  src={imgSrc}
-                  alt={title}
-                  title={title}
-                  fill
-                  className="object-cover object-top"
-                  onError={() => handleImageError(index)}
-                  unoptimized
-                />
+                {imgSrc && imgSrc.trim() !== "" && (
+                  <Image
+                    src={imgSrc}
+                    alt={title}
+                    title={title}
+                    fill
+                    className="object-cover object-top"
+                    onError={() => handleImageError(index)}
+                    unoptimized
+                  />
+                )}
               </a>
             </div>
           </div>
--- next.config.ts
+++ next.config.ts
@@ -78,7 +78,8 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     staticGenerationRetryCount: 2,
-    staticGenerationMaxConcurrency: 50,
+    staticGenerationMaxConcurrency: 200,
+    staticGenerationMinPagesPerWorker: 100,
   },
   async rewrites() {
     return [
