11932f09e80a1246e8bc9ac19e49d54428b29666
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-10-28 11:32:14 +1100

    adding tina-client

---
+++ app/tina-client.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useTina } from "tinacms/dist/react";
+export type UseTinaProps = {
+  query: string;
+  variables: object;
+  data: object;
+};
+
+export type TinaClientProps<T> = {
+  props: UseTinaProps & T;
+  Component: React.FC<{ tinaProps: { data: object }; props: T }>;
+};
+
+export function TinaClient<T>({ props, Component }: TinaClientProps<T>) {
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+
+  return <Component tinaProps={{ data }} props={{ ...props }} />;
+}
