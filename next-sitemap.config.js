e8117cbf1caece8d1f75f2c5bfaabe02c401f11c
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-11-19 15:41:29 +1100

    removing module type from package.json

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,7 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://ssw.com.au/rules",
   changefreq: "daily",
   priority: 0.7,
+  sitemapBaseFileName: "sitemap-index",
   sitemapSize: 5000,
   generateRobotsTxt: true,
   output: "standalone",
