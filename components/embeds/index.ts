a7d8b559364a3fc825a598598b2d502e3ea16224
Chloe Lin <chloelin@ssw.com.au>
2025-12-01 10:11:34 +0100

    add endOfIntro component to embed list

--- components/embeds/index.ts
+++ components/embeds/index.ts
@@ -1,26 +1,28 @@
 import { Template } from "tinacms";
-import { emailEmbedComponent,  emailEmbedTemplate } from "./emailEmbed";
-import { figureEmbedComponent,  figureEmbedTemplate } from "./figureEmbed";
-import { imageEmbedComponent,  imageEmbedTemplate } from "./imageEmbed";
 import { asideEmbedComponent, asideEmbedTemplate } from "./asideEmbed";
-import { youtubeEmbedComponent, youtubeEmbedTemplate } from "./youtubeEmbed";
+import { emailEmbedComponent, emailEmbedTemplate } from "./emailEmbed";
+import { endOfIntroComponent, endOfIntroTemplate } from "./endOfIntro";
+import { figureEmbedComponent, figureEmbedTemplate } from "./figureEmbed";
 import { introEmbedComponent, introEmbedTemplate } from "./IntroEmbed";
+import { imageEmbedComponent, imageEmbedTemplate } from "./imageEmbed";
+import { youtubeEmbedComponent, youtubeEmbedTemplate } from "./youtubeEmbed";
 
+export const embedComponents = {
+  ...emailEmbedComponent,
+  ...imageEmbedComponent,
+  ...figureEmbedComponent,
+  ...asideEmbedComponent,
+  ...youtubeEmbedComponent,
+  ...introEmbedComponent,
+  ...endOfIntroComponent,
+};
 
-export const embedComponents={
-    ...emailEmbedComponent,
-    ...imageEmbedComponent,
-    ...figureEmbedComponent,
-    ...asideEmbedComponent,
-    ...youtubeEmbedComponent,
-    ...introEmbedComponent,
-}
-
-export const embedTemplates:Template[]=[
-    emailEmbedTemplate,
-    imageEmbedTemplate,
-    figureEmbedTemplate,
-    asideEmbedTemplate,
-    youtubeEmbedTemplate,
-    introEmbedTemplate
-]
+export const embedTemplates: Template[] = [
+  emailEmbedTemplate,
+  imageEmbedTemplate,
+  figureEmbedTemplate,
+  asideEmbedTemplate,
+  youtubeEmbedTemplate,
+  introEmbedTemplate,
+  endOfIntroTemplate,
+];
